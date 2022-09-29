a6e2c42519b40e8d936274de3b88049a901ac9f5
lemoanana <liuzelin01@outlook.com>
2022-09-29 09:35:54 +0800

    新增，部分示例

--- l01/demo.py
+++ l01/demo.py
@@ -1,28 +1,36 @@
 from imaplib import Int2AP
-from operator import contains
+from operator import contains, index
 from ssl import SSLWantReadError
+from textwrap import indent
+from traceback import print_last
+
+'''注释写法： 
+当行一个 # ，
+vscode 快捷键： Ctrl + /
 
 
-name = '玛丽亚'
-age = 76
+'''
+# --------------------------------------------------------
+# name = '玛丽亚'
+# age = 76
 
 # print(type(name),type(age))
 # print('我叫'+ name +'，今年'+ str(age) +'岁')
 # print('------------------str() 将其他类型转成 str 类型---')
 
-a = 10
-b = 198.8
-c = False
+# a = 10
+# b = 198.8
+# c = False
 # print(type(a),type(b),type(c))
 # print("""str(a),str(b),str(c),
 # type(str(a)),type(str(b)),type(str(c))
 # """)
 # print('------------------int() 将其他类型转成 int 类型---')
-s1 = '123'
-f1 = 98.7
-s2 = '76.77'
-ff = True
-s3 = 'hello'
+# s1 = '123'
+# f1 = 98.7
+# s2 = '76.77'
+# ff = True
+# s3 = 'hello'
 # print (type(s1),type(f1),type(s2),type(ff),type(s3))
 # print (int(s1),type(int(s1)))
 # print (int(s1),type(int(s1)))
@@ -31,11 +39,11 @@ s3 = 'hello'
 # # print (int(s3),type(int(s3)))     # str 转成 int 类型时，字符串必须为数字串（整数）
 
 # print('------------------float() 将其他类型转成 float 类型---')
-s1 = '128.98'
-s2 = '76'
-ff = True
-s3 = 'hello'
-i = 98
+# s1 = '128.98'
+# s2 = '76'
+# ff = True
+# s3 = 'hello'
+# i = 98
 '''print (type(s1),type(s2),type(ff),type(s3),type(i))
 print(float(s1),type(float(s1)))
 print(float(s2),type(float(s2)))
@@ -187,11 +195,85 @@ print(float(i),type(float(i)))'''
 #         sum += i
 # print('1-100 的偶数和为： ',sum)
 
-print('输出 100 到 999 之间的水仙花数')
-for i in range(100,1000):
-    ge = i % 10
-    shi = i // 10 % 10
-    bai = i // 100
-    # print(bai,shi,ge)
-    if ge ** 3 + shi ** 3 + bai ** 3 == i:
-        print(i)
+# print('输出 100 到 999 之间的水仙花数')
+# for i in range(100,1000):
+#     ge = i % 10
+#     shi = i // 10 % 10
+#     bai = i // 100
+#     # print(bai,shi,ge)
+#     if ge ** 3 + shi ** 3 + bai ** 3 == i:
+#         print(i)
+
+# 从键盘录入密码，最多录入 3 次，如果正确就结束循环
+
+# for i in range(3):
+#     pwd = input('请输入密码： ')
+#     if pwd == '8888':
+#         print('密码正确')
+#         break
+#     else: 
+#         print('密码不正确')
+# while 写法
+# a = 0
+# while a < 3:
+#     pwd = input('请输入密码： ')
+#     a += 1
+#     if pwd == '8888':
+#         print('密码正确')
+#         break
+#     else: 
+#         print('密码不正确')
+# else:
+#     print('三次密码均输入错误')
+
+# 输出 1-50 所有 5 的倍数，
+# for i in range(1,51):
+#     if i % 5 == 0:
+#         print(i,' 为 5 的倍数')
+# # continue
+# for i in range(1,51):
+#     if i % 5 != 0:
+#         continue
+#     else:
+#         print(i,' 为 5 的倍数')
+
+'''输入一个三行四列的矩形'''
+# for i in range(3):
+#     for j in range(4):
+#         print('*',end='\t')     # 不换行输出
+#     print()     # 换行
+
+# for i in range(1,10):
+# #     print(i)
+#     for j in range(1,i+1):
+#         print(i,'*',j,'=',i*j,end='\t')
+#     print()s
+
+# for i in range(5):
+#     for j in range(1,11):
+#         if j % 2 == 0:
+#             continue
+#         print(j,end='\t')
+#     print()
+
+'''创建列表'''
+# lst = ['hello','world',98,'hello',234]      # 获取单个元素。正向索引： 从 0 到 N-1，逆向索引： 从 -N 到 -1 
+# print(lst)
+# print(lst.index('hello'))       # 列表中有相同元素，只返回列表中相同元素的第一个元素的索引
+# # print(lst.index('hello',1,3))       # 在 index 为1，2(不包括右边3) 中去查找字符串 'hello'，查不到所以抛出异常
+# print(lst[2])
+# print(lst[-3])
+
+# lst = [10,20,30,40,50,60,70,80,90,90,90]
+# # print(lst[1:6:])     # 切片，索引 1 到 5 的部分，步长为 1 
+# print(lst[1:6:2])
+# print(lst[:6:2])        # 默认开始，从索引为 0 开始
+# print(lst[1::2])        # 默认结束，到最后
+# print(lst[::-1])
+
+lst = [10,20,30,40]
+# print(10 in lst)
+
+
+
+
