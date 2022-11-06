2dc9e58f330a996f03455b0f2653bf4a708e539f
liuzelin <liuzelin@hilonggroup.com>
2022-11-06 18:12:55 +0800

    feat: 增加以下，实操案例

---
+++ l01/pratice/demo.py
@@ -0,0 +1,11 @@
+'''一、使用 print 方式进行输出（输出的目的地是文件）'''
+
+# 第一种方式
+# fp = open('./test.txt', 'w')
+# print('奋斗成就更好的你',file=fp)
+# fp.close()
+
+# 第二种方式
+# with open('./test.txt', 'w') as f:
+#     f.write('奋斗成就更好的你-with')
+
