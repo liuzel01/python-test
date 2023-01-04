b396d40cb43e70ea54a257016acf765dbe3a9da6
lemoanana <liuzelin01@outlook.com>
2023-01-04 11:45:49 +0800

    feat: 新增脚本： 爬取电影排名

--- devops/gitee_code.py
+++ devops/gitee_code.py
@@ -37,7 +37,7 @@ if  __name__ == "__main__":
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     url = "https://movie.douban.com/j/chart/top_list"
-    start_num = input("enter start num:")
+    start_num = int(input("enter start num:")) - 1
     limit_num = input("enter limit num:")
     param = {
         'type': '5',
@@ -53,29 +53,30 @@ if  __name__ == "__main__":
     # print('\r\n',ps_name)
     # 确认服务器返回的json则可以使用json
     dic_obj = response.json()
+    print(dic_obj)
     # json_name = json.loads(dic_obj)
-    # app = xw.App(visible=False, add_book=False)
-    app = xw.App(visible=True, add_book=False)
-    # wb = app.books.add()
-    # sht = wb.sheets['Sheet1']
+    app = xw.App(visible=False, add_book=False)
+    # app = xw.App(visible=True, add_book=False)
+    wb = app.books.add()
+    sht = wb.sheets['Sheet1']
 
-    # sht.range('A1').value ='序号'
-    # sht.range('B1').value ='排名'
-    # sht.range('C1').value ='名称'
-    # sht.range('D1').value ='评分'
-    # sht.range('E1').value ='上映日期'
-    # sht.range('F1').value ='国家'
-    #
-    # i=1
-    # for dic_item in dic_obj:
-    #     print('cnt:',i,'title ',dic_item['title'],'\r\n')
-    #     sht.range('A'+str(i+1)).value = i
-    #     sht.range('B'+str(i+1)).value = dic_item['rank']
-    #     sht.range('C'+str(i+1)).value = dic_item['title']
-    #     sht.range('D'+str(i+1)).value = dic_item['rating'][0]
-    #     sht.range('E'+str(i+1)).value = dic_item['release_date']
-    #     sht.range('F'+str(i+1)).value = dic_item['regions'][0]
-    #     i += 1
-    # wb.save('movie_rank.xlsx')
-    # wb.close()
-    # app.quit()
+    sht.range('A1').value ='序号'
+    sht.range('B1').value ='排名'
+    sht.range('C1').value ='名称'
+    sht.range('D1').value ='评分'
+    sht.range('E1').value ='上映日期'
+    sht.range('F1').value ='国家'
+
+    i = 1
+    for dic_item in dic_obj:
+        print('cnt:',i,'title ',dic_item['title'],'\r\n')
+        sht.range('A'+str(i+1)).value = i
+        sht.range('B'+str(i+1)).value = dic_item['rank']
+        sht.range('C'+str(i+1)).value = dic_item['title']
+        sht.range('D'+str(i+1)).value = dic_item['rating'][0]
+        sht.range('E'+str(i+1)).value = dic_item['release_date']
+        sht.range('F'+str(i+1)).value = dic_item['regions'][0]
+        i += 1
+    wb.save('movie_rank.xlsx')
+    wb.close()
+    app.quit()
