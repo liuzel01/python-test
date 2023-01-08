2922c8057eb5c0a45759bb17440fd64553517d93
liuzelin <liuzelin@hilonggroup.com>
2023-01-08 23:55:46 +0800

    新增“自动进入企业，并通过点击空白处隐藏弹窗”

--- devops/gitee_login.py
+++ devops/gitee_login.py
@@ -12,20 +12,29 @@ import time
 # 实现无可视化界面
 from selenium.webdriver.chrome.options import Options
 
+'''
+实现思路：
+1. 自动登录网站（模拟人的行为进行登录、浏览）
+2. 通过点击对应按钮，进入到填写日报页
+3. 点击“新建任务”按钮，开始填写日报
+4. 将对应字段填写完成，点击“新建”按钮，完成日报的创建'''
 
 if __name__ == "__main__":
 
     # 规避相应的检测
     chrome_options = Options()
     chrome_options.add_argument("--disable-blink-features=AutomationControlled --headless")
+
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("detach",True)
+
     # 这里填入你自己的chromedriver的安装路径
     driver = webdriver.Chrome(executable_path='C:/Program Files/Google/Chrome/Application/chromedriver.exe',options=chrome_options)
 
     driver.get('https://gitee.com/login')
+    driver.maximize_window()
 
     # 根据id获取用户账号输入框、密码输入框
-    # username_tag = driver.find_element_by_id('user_login')
-    # password_tag = driver.find_element_by_id('user_password')
     username_tag = driver.find_element(By.ID,'user_login')
     password_tag = driver.find_element(By.ID,'user_password')
 
@@ -34,32 +43,33 @@ if __name__ == "__main__":
     time.sleep(1)
     password_tag.send_keys('LIUzeLIN678^&*<>?')
 
-    # 根据id获取登录按钮
-    # login_btn = driver.find_element_by_id('J-login')
+    # 根据xpath获取登录按钮
     login_btn = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[1]/div[2]/div[1]/form[1]/div/div/div/div[4]/input')
-
     # 点击登录按钮
     login_btn.click()
-    # 这里必须得休眠，不然运行速度太夸，代码难以定位到滑块
-    # time.sleep(5)
-    # # span = driver.find_element_by_css_selector('.btn_slide')
-    # span = driver.find_element(By.CSS_SELECTOR,'.btn_slide')
 
-    # gitee 上无需滑块以及动作链
-    # # 定义动作链，点击并拖拽
-    # aco = ActionChains(driver)
-    # # 点击并长按
-    # aco.click_and_hold(span)
-    #
-    # # perform()立即执行动作链操作
-    # for i in range(5):
-    #     aco.move_by_offset(25,0).perform()
-    #     time.sleep(0.3)
-    #
-    # # 释放动作链
-    # aco.release()
+    # 进入企业
+    time.sleep(5)
+    # hlit_btn = driver.find_element(By.XPATH,'/html/body/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/a')
+    # 根据元素内容来定位
+    hlit_btn = driver.find_element(By.XPATH,"//h4[contains(text(),'海隆石油集团（上海）信息技术有限公司')]")
+    hlit_btn.click()
 
-    time.sleep(2)
+    # 有新打开窗口，需要切换窗口
+    windows = driver.window_handles
+    driver.switch_to.window(windows[1])
+
+    # 点击空白处，隐藏弹窗
+    action = ActionChains(driver)
+    time.sleep(10)
+    action.move_by_offset(100,100).click().perform()
+    # driver.get('https://e.gitee.com/hilong_1/dashboard')
+    # # 进入工作项
+    # time.sleep(3)
+    # issue_btn = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[1]/div[2]/div[3]/a[3]')
+    # issue_btn.click()
+
+    # time.sleep(2)
     # ok_btn = driver.find_element_by_css_selector('.ok')
     # ok_btn = driver.find_element(By.CSS_SELECTOR,'.ok')
     # ok_btn.click()
@@ -68,6 +78,6 @@ if __name__ == "__main__":
     # time.sleep(5)
     # driver.quit()
 
-    # 在执行完动作后，不关闭浏览器
-    browser = webdriver.Chrome()
-    ActionChains(browser).key_down(Keys.CONTROL).send_keys("t").key_up(Keys.CONTROL).perform()
+    # # 在执行完动作后，不关闭浏览器
+    # # browser = webdriver.Chrome()
+    # ActionChains(driver).key_down(Keys.CONTROL).send_keys("t").key_up(Keys.CONTROL).perform()
