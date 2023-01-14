87a0191ac0aae71b2e93c26a10a425831deefea8
liuzelin <liuzelin@hilonggroup.com>
2023-01-14 22:13:41 +0800

    新增进入日报页、填写标题、负责人、类型、关联项目等信息

--- devops/gitee_login.py
+++ devops/gitee_login.py
@@ -21,7 +21,8 @@ from selenium.webdriver.chrome.options import Options
 '''
 
 # todo(liuzel01): 跳转到填写日报页面，并打开新建任务页
-# todo(liuel01): 通过读取本地文件，填写任务的字段
+# todo(liuzel01): 通过读取本地文件，填写任务的字段
+# todo(liuzel01): 将函数 time.sleep 使用 while(1): try: 循环代替
 
 if __name__ == "__main__":
 
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     time.sleep(1)
     password_tag.send_keys('LIUzeLIN678^&*<>?')
 
-    # 根据xpath获取登录按钮
+    # 通过xpath获取登录按钮
     login_btn = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[1]/div[2]/div[1]/form[1]/div/div/div/div[4]/input')
     # 点击登录按钮
     login_btn.click()
@@ -65,8 +66,44 @@ if __name__ == "__main__":
 
     # 点击空白处，隐藏弹窗
     action = ActionChains(driver)
-    time.sleep(10)
+    time.sleep(15)
     action.move_by_offset(100,100).click().perform()
+
+    # 进入填写日报页
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[1]/div[2]/div[3]/a[3]').click()
+    # 进入任务
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[3]/div/div/div/div[1]/div[1]/div/a[3]').click()
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[3]/div/div/div/div[1]/a[1]').click()
+
+    # 标题
+    time.sleep(5)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[1]/div/div/input').send_keys('l01-日报')
+    # driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[2]/div[1]/div/div/div/div').send_keys('刘泽霖')
+    # 负责人/协作者
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[2]/div[1]/div/div/div/div').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[2]/div[1]/div/div[2]/div/div[1]/div/input').send_keys('liuzelin')
+    time.sleep(3)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[2]/div[1]/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/span[1]').click()
+
+    # 类型是下拉选项框，这里先选择父节点，再触发子节点点击
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[2]/div[2]/div/div/span[1]/input').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[3]/div/div/div/div[2]/div[1]/div/div/div[6]/div').click()
+    # parent.find_element_by_xpath('.//option[@value="20"]').click()
+    # 关联项目
+    # driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[3]/div[1]/div/div/span[1]/input').click()
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[3]/div[1]/div/div/span[1]/input').send_keys('软件产品部-日报')
+    time.sleep(3)
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[3]/div/div/div/div[2]/div[1]/div/div/div[2]').click()
+
+    # 计划时间
+    current_time = time.strftime('%Y.%m.%d',time.localtime(time.time()))
+    driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[3]/div[2]/div/div/div/div/div[1]/input').send_keys(current_time)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[3]/div[2]/div/div/div/div/div[3]/input').send_keys(current_time)
+
+    # 开始写入任务描述
+    driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[4]/div[1]/div/div/div[1]/form/div[1]/div[4]/div/div[1]').click()
     # driver.get('https://e.gitee.com/hilong_1/dashboard')
     # # 进入工作项
     # time.sleep(3)
